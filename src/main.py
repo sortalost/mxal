5adb4716a86c818afc26dbef233e0f9ab27d75f1
Sorta L⨀SŦ <aditya.funs.11@gmail.com>
2025-08-08 16:21:29 +0530

    smh

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify
 from .modules.imap_client import fetch_inbox, test_login, fetch_email
 from .modules.smtp_client import send_email
 from .modules.utils import login_required
