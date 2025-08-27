f9b84adce626e4ecce980f4ec437c161afd862b6
Sorta L⨀SŦ <aditya.funs.11@gmail.com>
2025-08-27 14:30:26 +0530

    add date

--- src/modules/smtp_client.py
+++ src/modules/smtp_client.py
@@ -3,6 +3,7 @@ import imaplib
 import time
 from flask import flash
 from email.mime.text import MIMEText
+from email.utils import formatdate
 from .imap_client import IMAP_PORT, IMAP_SERVER
 
 SMTP_SERVER = "mail.cock.li"
@@ -13,6 +14,7 @@ def send_email(user, password, to_address, subject, body):
     msg["Subject"] = subject
     msg["From"] = user
     msg["To"] = to_address
+    msg["Date"] = formatdate(localtime=True)
     with smtplib.SMTP_SSL(SMTP_SERVER, SMTP_PORT) as server:
         server.login(user, password)
         server.sendmail(user, [to_address], msg.as_string())
